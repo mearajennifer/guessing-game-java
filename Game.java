7e24701faf7ad4e20bb3a30b5e0a704c62bbda59
Jennifer Meara <jennifer@jmeara.com>
2021-06-01 15:57:10 +0000

    add greeting and get player name

--- Game.java
+++ Game.java
@@ -4,10 +4,19 @@ A number-guessing game.
 
 */
 
+import java.util.Scanner;
+import java.util.Random;
+
+
 public class Game {
   public static void main(String[] args){
-    // Put your code here
-    System.out.println("Hello World");
+
+    Scanner input = new Scanner(System.in);
+    
+    // Greet player
+    System.out.println("Howdy, what's your name?");
+    String playerName = input.nextLine();
+
   }
 
 }
